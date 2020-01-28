8d695bac25e088112db95227527c053c9880dfef
jan <declercqjan@gmail.com>
2020-01-28 14:41:13 +0100

    added function to calculate difference between now and 20 minutes in the future, yet it recalculateswith every refresh

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import Header from "../components/Header";
 import Timer from "../components/Timer";
-import CountdownTimer from "../components/CountdownTimer";
+// import CountdownTimer from "../components/CountdownTimer";
 
-const date = new Date();
-console.log(date);
-date.setMinutes(20);
-console.log(date);
+const date1 = new Date();
+console.log(date1);
+const date2 = new Date();
+date2.setMinutes(date1.getMinutes()+20);
+console.log(date2);
 // ik denk niet dat dit zal werken, want getMinutes geeft integer terug. Ik denk eerder dat ik zal moeten rekenen
 /*
 console.log(date);
@@ -15,11 +16,31 @@ const twentyMinutes = date.getMinutes();
 console.log(twentyMinutes);
  */
 
+const calculateTimeLeft = (date1, date2) => {
+  console.log(date1);
+  console.log(date2);
+  // const difference = +new Date("2020-01-01") - +new Date();
+  const difference = date2 - date1;
+  let timeLeft = {};
+  console.log(difference);
+  if (difference > 0) {
+    timeLeft = {
+      // days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      // hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60)
+    };
+  }
+
+  return timeLeft;
+};
+console.log(calculateTimeLeft(date1, date2));
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      timeLeft: date,
+      // timeLeft: date,
       running: false,
       timerEnded: false
     };
@@ -29,10 +50,8 @@ class App extends React.Component {
       <div>
         <Header />
         <main>
-        <div>
-            <CountdownTimer/>
-          </div>
-          <Timer timeLeft={this.state.timeLeft} />
+          <div>{/* <CountdownTimer/> */}</div>
+          {/* <Timer timeLeft={this.state.timeLeft} /> */}
         </main>
         <div className="container">
           <h1>Hello {this.props.name}</h1>
